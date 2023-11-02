4d7ffa3a207e77c29a0bec92397741c8d4b2c208
Joice Ferreira Passos <54285614+joicepassos@users.noreply.github.com>
2023-11-02 14:36:10 -0300

    New navbar components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
-import React from 'react';
+import { useState } from 'react';
 import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from './scenes/Navbar';
+import DotGroup from "./scenes/DotGroup";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
   return (
-    <div className="App bg-deep-blue">
-     
-     
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+       
+      </div>
     </div>
   );
 }
